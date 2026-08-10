f4575e25f686a4564c83a7c75d7d62f2d6265d50
ouriel <pulse010@proton.me>
2026-08-10 18:26:32 -0400

    OKAY PLEASE.

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ouriel/Projects/Git/ProjectHarmony
+# Install script for directory: /home/ouriel/Projects/ProjectHarmony
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/ouriel/Projects/Git/ProjectHarmony/build/install_local_manifest.txt"
+  file(WRITE "/home/ouriel/Projects/ProjectHarmony/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/ouriel/Projects/Git/ProjectHarmony/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/ouriel/Projects/ProjectHarmony/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
